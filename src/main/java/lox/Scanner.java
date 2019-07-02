43c6cc1c42e96b75ce9960eb0db06de39984d207
Paul <paul.naranja@gmail.com>
2019-07-01 23:04:01 -0700

    Add methods to scan tokens

--- src/main/java/lox/Scanner.java
+++ src/main/java/lox/Scanner.java
@@ -3,14 +3,14 @@ package lox;
 import java.util.ArrayList;
 import java.util.List;
 
-import static lox.TokenType.EOF;
+import static lox.TokenType.*;
 
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
 
     private int start = 0;
-    private int line = 0;
+    private int line = 0;   // line number
     private int current = 0;
 
     Scanner(String src) {
@@ -44,9 +44,74 @@ public class Scanner {
 
     /**
      * Identify a token given a TokenType
+     * For now, lets scan one character at a time
      */
     private void scanToken() {
+        char c = advance();
+        switch (c) {
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+            default:
+                Lox.error(line, "Unrecognized character");
+        }
+    }
 
+    /**
+     * Increment the current "pointer" and return the next character
+     *
+     * @return The next character in the source
+     */
+    private char advance() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    /**
+     * Add a token given the type and possible literal
+     *
+     * @param type    The Token Type defined in the TokenType enum
+     * @param literal The possible literal value (number/string)
+     */
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    /**
+     * Add token with no literal (number/string)
+     *
+     * @param type The Token Type defined in the TokenType enum
+     */
+    private void addToken(TokenType type) {
+        addToken(type, null);
     }
 
 
--- src/main/java/lox/Token.java
+++ src/main/java/lox/Token.java
@@ -3,7 +3,7 @@ package lox;
 public class Token {
     final TokenType type;
     final String lexeme;
-    final Object literal;
+    final Object literal; // For the actual numbers/strings.  Convert to actual type later
     final int line;
 
     Token(TokenType type, String lexeme, Object literal, int line) {
